d74446cc1705fffd723165c472b3035b2235f2fd
JonathanAstorga <jonathan.astorga245791@potros.itson.edu.mx>
2025-05-16 23:45:24 -0700

    botones

--- app/src/main/java/astorga/jonathan/prototipopfdsi/PrincipalActivity.kt
+++ app/src/main/java/astorga/jonathan/prototipopfdsi/PrincipalActivity.kt
@@ -1,6 +1,9 @@
 package astorga.jonathan.prototipopfdsi
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.animation.AnimationUtils
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,45 @@ class PrincipalActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val animScale = AnimationUtils.loadAnimation(this, R.anim.anim_press_button)
+
+        val btnHome: ImageButton = findViewById(R.id.btnHome)
+        val btnMap: ImageButton = findViewById(R.id.btnMap)
+        val btnRese: ImageButton = findViewById(R.id.btnRese)
+        val btnPerfil: ImageButton = findViewById(R.id.btnPerfil)
+
+        btnHome.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> v.startAnimation(animScale)
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> v.clearAnimation()
+            }
+            false
+        }
+
+        btnMap.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> v.startAnimation(animScale)
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> v.clearAnimation()
+            }
+            false
+        }
+
+        btnRese.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> v.startAnimation(animScale)
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> v.clearAnimation()
+            }
+            false
+        }
+
+        btnPerfil.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> v.startAnimation(animScale)
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> v.clearAnimation()
+            }
+            false
         }
     }
 }
